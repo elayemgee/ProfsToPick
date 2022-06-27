8ecb9a7f9a854e49f5a266cc33d70be31407c9b9
elayemgee <65518364+elayemgee@users.noreply.github.com>
2022-06-27 11:00:07 +0900

    connect to localhost:9090

--- index.js
+++ index.js
@@ -6,13 +6,15 @@ const routes = require(`./routes/routes.js`);
 const db = require(`./models/db.js`);
 
 const app = express();
+const port = 9090;
 app.use(bodyParser.urlencoded({ extended: false }));
+
 
 app.set(`view engine`, `hbs`);
 hbs.registerPartials(__dirname + `/views/partials`);
 
 dotenv.config();
-port = process.env.PORT;
+//port = process.env.PORT;
 hostname = process.env.HOSTNAME;
 
 app.use(express.static(`public`));
@@ -22,5 +24,6 @@ db.connect();
 
 app.listen(port, hostname, function () {
     console.log(`Server is running at:`);
-    console.log(`http://` + hostname + `:` + port);
+    console.log(`http://` + `ProfsToPick` + `:` + port);
+    //console.log(`http://` + hostname + `:` + port);
 });
