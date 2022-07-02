6490238cfb026b774f8c13dfeb29dbd5172f9569
lei-a <104745908+lei-a@users.noreply.github.com>
2022-07-02 22:16:36 +0800

    commit

--- routes/routes.js
+++ routes/routes.js
@@ -69,7 +69,7 @@ app.get(`/coec`, collegeController.getCOEC);
 
 //making a review
 app.get(`/profpage`, reviewController.getReview); //landing on the review page
-app.post(`/profpage`, reviewController.postReview); //posting the review details on prof's page and user's page
+app.post(`/postReview`, reviewController.postReview); //posting the review details on prof's page and user's page
 
 module.exports = app;
 
