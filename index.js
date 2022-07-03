dc8e6a8de429f38a9f19617bd97312dfd51d0d44
elayemgee <65518364+elayemgee@users.noreply.github.com>
2022-07-03 16:54:54 +0900

    make it better

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const dotenv = require(`dotenv`);
 const express = require(`express`);
 const mongoose = require(`mongoose`);
 const app = express();
@@ -20,26 +21,44 @@ const MongoStore = require('connect-mongo');
 
 app.use(express.urlencoded({extended: true}));
 
+/*
 app.use(session({
     'secret': 'sikret',
     'resave': false,
     'saveUninitialized': false,
     store: MongoStore.create({
         mongoUrl: 'mongodb://localhost:27017/ccapdev-profstopick'}),
-  }));
+  }));*/
+
+app.use(session({
+    'secret': 'ccapdev-session',
+    'resave': false,
+    'saveUninitialized': false,
+    store: new MongoStore({mongooseConnection: mongoose.connection})
+}));
 
 app.use(`/`, routes);
 
-hostname = process.env.HOSTNAME;
+//hostname = process.env.HOSTNAME;
+db.connect();
 
 mongoose.connection.on('connected', () => {
     console.log('Mongoose Connection');
 });
-
-db.connect();
-
-app.listen(port, hostname, function () {
-    console.log(`Server is running at:`);
-    console.log(`http://` + `localhost` + `:` + port);
+/*
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/' + 'login.hbs');
 });
+*/
+
+app.listen(PORT, function () {
+    console.log('app listening at port ' + PORT);
+});
+
+/*
+app.listen(PORT, hostname, function () {
+    console.log(`Server is running at:`);
+    console.log(`http://` + `localhost` + `:` + PORT);
+});
+*/
 
