d9ff6104f0c7bd79f828a14227ca0283a4406b96
lei-a <104745908+lei-a@users.noreply.github.com>
2022-07-03 21:18:00 +0800

    Fixed connection

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 require('dotenv').config();
 
 const mongoose = require(`mongoose`);
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/ccapdev-profstopick');
+mongoose.createConnection(process.env.MONGODB_URI || 'mongodb://localhost:27017/ccapdev-profstopick');
 
 const session = require('express-session');
 const hbs = require(`hbs`);
